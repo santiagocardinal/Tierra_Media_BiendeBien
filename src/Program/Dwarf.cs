d279fbded43908e9b8d4c83e6bbd1927309d6663
Camila Orue <camila.orue@correo.ucu.edu.uy>
2025-09-17 16:42:33 -0300

    Se crea la clase Dwarf

---
+++ src/Program/Dwarf.cs
@@ -0,0 +1,82 @@
+namespace Program;
+
+public class Dwarf
+{
+    private string name;
+    private List<Item> element;
+    private int amountLife;
+    public static int initialLife { get; set; } = 50;
+
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public List<Item> Element
+    {
+        get { return element; }
+        set { element = value; }
+    }
+
+    public int AmountLife
+    {
+        get { return amountLife; }
+        set { amountLife = value; }
+    }
+
+    public Dwarf(string name, List<Item> elements)
+    {
+        this.Name = name;
+        this.Element = elements ?? new List<Item>();
+        this.AmountLife = initialLife;
+    }
+
+    public void AddItem(Item element)
+    {
+        this.Element.Add(element);
+    }
+
+    public void RemoveItem(Item element)
+    {
+        this.Element.Remove(element);
+    }
+
+    public void ExchangeItem(Item e1, Item e2)
+    {
+        int index = this.Element.IndexOf(e1);
+        if (index != -1)
+        {
+            this.Element[index] = e2;
+        }
+    }
+
+    public void Heal()
+    {
+        this.AmountLife = initialLife;
+    }
+
+    public void DwarfElf(Elf elf)
+    {
+        foreach (var item in elf.Element)
+        {
+            this.AmountLife -= item.DamageValue;
+        }
+    }
+    public void DwarfWizard(Wizard wizard)
+    {
+        foreach (var item in wizard.Element)
+        {
+            this.AmountLife -= item.DamageValue;
+        }
+    }
+    public void DwarfDwarf(Dwarf dwarf)
+    {
+        foreach (var item in dwarf.Element)
+        {
+            this.AmountLife -= item.DamageValue;
+        }
+    }
+    
+}
